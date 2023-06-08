a580c7087bde701885b757f5c8064b0acf8980cc
Erkin Çak <102376494+erkincak@users.noreply.github.com>
2023-06-08 21:13:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ToDoList
+{
+    static List<string> tasks = new List<string>();
+
+    static void Main()
+    {
+        bool isRunning = true;
+
+        while (isRunning)
+        {
+            Console.WriteLine("===== To-Do List =====");
+            Console.WriteLine("1. Add Task");
+            Console.WriteLine("2. View Tasks");
+            Console.WriteLine("3. Mark Task as Completed");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("");
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    AddTask();
+                    break;
+                case "2":
+                    ViewTasks();
+                    break;
+                case "3":
+                    MarkTaskAsCompleted();
+                    break;
+                case "4":
+                    isRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static void AddTask()
+    {
+        Console.Write("Enter the task: ");
+        string task = Console.ReadLine();
+        tasks.Add(task);
+        Console.WriteLine("Task added successfully.");
+    }
+
+    static void ViewTasks()
+    {
+        if (tasks.Count == 0)
+        {
+            Console.WriteLine("No tasks in the list.");
+        }
+        else
+        {
+            Console.WriteLine("Tasks:");
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {tasks[i]}");
+            }
+        }
+    }
+
+    static void MarkTaskAsCompleted()
+    {
+        if (tasks.Count == 0)
+        {
+            Console.WriteLine("No tasks in the list.");
+        }
+        else
+        {
+            Console.Write("Enter the task number to mark as completed: ");
+            int taskNumber;
+            if (int.TryParse(Console.ReadLine(), out taskNumber) && taskNumber >= 1 && taskNumber <= tasks.Count)
+            {
+                tasks.RemoveAt(taskNumber - 1);
+                Console.WriteLine("Task marked as completed.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid task number. Please try again.");
+            }
+        }
+    }
+}
